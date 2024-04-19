fefa804e96c7015d4dd81d96c8d75441693d4be8
Ahmdpolash <ahmedpolash732@gmail.com>
2024-04-19 23:19:12 +0600

    banner updated

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Container from "../container/Container";
+import Image from "next/image";
+import img from "@/assest/card.jpg";
 
 const Banner = () => {
   return (
-    <div className=" h-[calc(100vh_-_90px)] w-full flex items-center ">
+    <div className=" h-[calc(100vh_-_30px)] w-full flex items-center ">
       <Container>
-        <div className="flex items-center justify-between">
-          <div className="text-white space-y-1 lg:space-y-3">
+        <div className="w-ful flex justify-between">
+          <div className="text-white lg:w-[52vw] mt-5  lg:mt-7 space-y-1 lg:space-y-3">
             <div className="font-medium text-xl md:text-4xl lg:text-6xl flex flex-col tracking-wide space-y-1 ">
               <span> Monetize Webcam</span>
               <span> traffic and level up</span>
@@ -22,7 +24,88 @@ const Banner = () => {
               </button>
             </div>
           </div>
-          <div></div>
+
+          <div className="lg:w-[37vw]">
+            <div className="flex gap-7 mb-5">
+              <div
+                style={{
+                  backgroundImage: `url("https://www.trafee.com/wp-content/uploads/2024/03/inside.jpg")`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[275px]  w-full relative rounded-2xl"
+              >
+                <div className="absolute p-5 lg:p-6 inset-0 bg-black rounded-2xl bg-opacity-50 flex ">
+                  <div div className="text-white ">
+                    <h2 class="text-[22px] tracking-wider font-bold mb-20">
+                      Join the IGaming <br /> with Geekteams
+                    </h2>
+                    <button className="bg-white hover:bg-[#F79A43] hover:text-white duration-300 mt-6 text-black py-[6px] px-4 rounded-md">
+                      Read Now
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                style={{
+                  backgroundImage: `url("https://www.trafee.com/wp-content/uploads/2023/12/igaming_7.jpg")`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[275px] w-full relative rounded-2xl"
+              >
+                <div className="absolute inset-0 bg-black rounded-2xl bg-opacity-50 flex items-center justify-center">
+                  <div div className="text-white ">
+                    <h2 class="text-[22px] tracking-wider font-bold mb-16">
+                      Geekteams Guide to <br /> the IGaming <br /> Industry in
+                      2024
+                    </h2>
+                    <button className="bg-white hover:bg-[#F79A43] hover:text-white duration-300 mt- text-black py-[6px] px-4 rounded-md">
+                      Read Now
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-7">
+              <div
+                style={{
+                  backgroundImage: `url("https://www.trafee.com/wp-content/uploads/2023/12/igaming_7.jpg")`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[275px] w-full relative rounded-2xl"
+              >
+                <div className="absolute inset-0  p-5 lg:p-6  bg-black rounded-2xl bg-opacity-50 ">
+                  <div div className="text-white ">
+                    <h2 class="text-[22px] tracking-wider font-bold mb-20">
+                      Technologies <br /> shipping iGaming <br /> Future
+                    </h2>
+                    <button className="bg-white hover:bg-[#F79A43] hover:text-white duration-300 mt-6 text-black py-[6px] px-4 rounded-md">
+                      Read Now
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div
+                style={{
+                  backgroundImage: `url("https://www.trafee.com/wp-content/uploads/2023/11/trafee-newcomer-pack.jpg")`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[275px] w-full relative rounded-2xl"
+              >
+                <div className="absolute  p-5 lg:p-6  inset-0 bg-black rounded-2xl bg-opacity-50">
+                  <div div className="text-white ">
+                    <h2 class="text-[22px] tracking-wider font-bold mb-20">
+                      NewComer Pack - <br /> Bonuses for new <br /> Affiliates
+                    </h2>
+                    <button className="bg-white hover:bg-[#F79A43] hover:text-white duration-300 mt-6 text-black py-[6px] px-4 rounded-md">
+                      Read Now
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
